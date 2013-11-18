43f41198848ff4aa6d3efd9a20ed625e16edba31
Armand Leclercq <marman.email@gmail.com>
2013-11-18 14:51:33 +0100

    Create add forward

---
+++ add.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  add.hh
+ * \date  Mon 18 Nov 2013 02:49:43 PM CET
+ */
+
+#ifndef ADD_HH_
+# define ADD_HH_
+
+namespace maths
+{
+  template <typename lhs, typename rhs>
+  struct add {};
+} /* maths */
+
+#endif /* !ADD_HH_ */
