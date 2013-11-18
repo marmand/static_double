a534a60557eb0cdbbc9a4ce5f5769b36ef354c9a
Armand Leclercq <marman.email@gmail.com>
2013-11-18 14:52:12 +0100

    Create mult forward

---
+++ mult.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  mult.hh
+ * \date  Mon 18 Nov 2013 02:51:38 PM CET
+ */
+
+#ifndef MULT_HH_
+# define MULT_HH_
+
+namespace maths
+{
+  template <typename lhs, typename rhs>
+  struct mult {};
+} /* maths */
+
+#endif /* !MULT_HH_ */
