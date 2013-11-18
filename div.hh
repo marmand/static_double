c16021c1efad4565757cdc10c88d7f88f8c4c675
Armand Leclercq <marman.email@gmail.com>
2013-11-18 22:49:01 +0100

    Create div algorithm

---
+++ div.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/div.hh
+ * \date  Mon 18 Nov 2013 10:32:10 PM CET
+ */
+
+#ifndef MATHS_DIV_HH_
+# define MATHS_DIV_HH_
+
+namespace maths
+{
+  template <typename lhs, typename rhs>
+  struct div {};
+} /* maths */
+
+#endif /* !MATHS_DIV_HH_ */
---
+++ long/div.hh
@@ -0,0 +1,19 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/long/div.hh
+ * \date  Mon 18 Nov 2013 10:33:37 PM CET
+ */
+
+#ifndef MATHS_LONG_DIV_HH_
+# define MATHS_LONG_DIV_HH_
+
+namespace maths
+{
+  template <long v1, long v2>
+  struct div<Long<v1>, Long<v2>>
+  {
+    typedef Long<v1 / v2> type;
+  };
+} /* maths */
+
+#endif /* !MATHS_LONG_DIV_HH_ */
