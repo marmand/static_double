38baeed4f1300abc5d60ecbe8330741b0de6f4fa
Armand Leclercq <marman.email@gmail.com>
2013-11-18 14:43:50 +0100

    Create back long and double types

---
+++ double.hh
@@ -0,0 +1,10 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  double.hh
+ * \date  Mon 18 Nov 2013 02:42:39 PM CET
+ */
+
+#ifndef DOUBLE_HH_
+# define DOUBLE_HH_
+# include "double/type.hh"
+#endif /* !DOUBLE_HH_ */
---
+++ long.hh
@@ -0,0 +1,10 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  long.hh
+ * \date  Mon 18 Nov 2013 02:43:25 PM CET
+ */
+
+#ifndef LONG_HH_
+# define LONG_HH_
+# include "long/type.hh"
+#endif /* !LONG_HH_ */
