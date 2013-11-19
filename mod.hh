36be060055362576eb2b01b26b7f2c0c6e3caaed
Armand Leclercq <marman.email@gmail.com>
2013-11-19 12:44:04 +0100

    Complete maths operation collection

---
+++ mod.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/mod.hh
+ * \date  Tue 19 Nov 2013 12:42:57 PM CET
+ */
+
+#ifndef MATHS_MOD_HH_
+# define MATHS_MOD_HH_
+
+namespace maths
+{
+  template <typename lhs, typename rhs>
+  struct mod {};
+} /* maths */
+
+#endif /* !MATHS_MOD_HH_ */
---
+++ sub.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/sub.hh
+ * \date  Tue 19 Nov 2013 12:41:50 PM CET
+ */
+
+#ifndef MATHS_SUB_HH_
+# define MATHS_SUB_HH_
+
+namespace maths
+{
+  template <typename lhs, typename rhs>
+  struct sub {};
+} /* maths */
+
+#endif /* !MATHS_SUB_HH_ */
