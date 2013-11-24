fb631cba4f6ac82a91bb400d408d0bd269d7e6c4
Armand Leclercq <marman.email@gmail.com>
2013-11-24 18:43:25 +0100

    Create first abs file

---
+++ abs.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/abs.hh
+ * \date  Sun 24 Nov 2013 06:36:52 PM CET
+ */
+
+#ifndef MATHS_ABS_HH_
+# define MATHS_ABS_HH_
+
+namespace maths
+{
+  template <typename lhs>
+  struct abs {};
+} /* maths */
+
+#endif /* !MATHS_ABS_HH_ */
