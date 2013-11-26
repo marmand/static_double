e654417e5c59b5dc6b096242718fa0f941efdcd3
Armand Leclercq <marman.email@gmail.com>
2013-11-27 00:13:59 +0100

    Remove useless empty lines

--- long/add.hh
+++ long/add.hh
@@ -10,13 +10,11 @@
 
 namespace maths
 {
-
   template <long v1, long v2>
   struct add<Long<v1>, Long<v2>>
   {
     typedef Long<v1 + v2> type;
   };
-
 } /* maths */
 
 #endif /* !MATHS_LONG_ADD_HH_ */
