ec899fd4d2b85d8578c3628223b43854e4958406
Armand Leclercq <marman.email@gmail.com>
2013-11-26 19:20:28 +0100

    Fix add algorithm

--- double/add.hh
+++ double/add.hh
@@ -8,12 +8,89 @@
 # define MATHS_DOUBLE_ADD_HH_
 # include <maths/double/type.hh>
 # include <maths/max.hh>
+# include <maths/abs.hh>
+# include <maths/add.hh>
+# include <maths/sub.hh>
+# include <maths/mult.hh>
 # include <maths/div.hh>
 # include <maths/mod.hh>
 # include <maths/pow.hh>
 
 namespace maths
 {
+  namespace details
+  {
+
+    /*!
+     * This structure compute a temporary variables on condition.
+     *
+     * if m1 < m2
+     *   d = d1 * 10 ^ |m1 - m2| + d2;
+     * else
+     *   d = d1 + d2 * 10 ^ |m1 - m2|;
+     */
+    template
+    <
+      unsigned long d1
+      , unsigned long d2
+      , unsigned long m1
+      , unsigned long m2
+      , bool inf
+    >
+    struct double_d_compute {};
+
+    template
+    <
+      unsigned long d1
+      , unsigned long d2
+      , unsigned long m1
+      , unsigned long m2
+    >
+    struct double_d_compute<d1, d2, m1, m2, true>
+    {
+      typedef typename maths::add
+              <
+                typename maths::mult
+                <
+                  Long<d1>
+                  , typename maths::pow
+                    <
+                      Long<10>
+                      , typename maths::abs<typename maths::sub<Long<m1>, Long<m2>>::type>::type
+                    >::type
+                >::type
+                , Long<d2>
+              >::type
+              type;
+    };
+
+    template
+    <
+      unsigned long d1
+      , unsigned long d2
+      , unsigned long m1
+      , unsigned long m2
+    >
+    struct double_d_compute<d1, d2, m1, m2, false>
+    {
+      typedef typename maths::add
+              <
+                Long<d1>
+                , typename maths::mult
+                  <
+                    Long<d2>
+                    , typename maths::pow
+                      <
+                        Long<10>
+                        , typename maths::abs<typename maths::sub<Long<m1>, Long<m2>>::type>::type
+                      >::type
+                  >::type
+              >::type
+              type;
+    };
+
+  } /* details */
+
   template
   <
     long e1
@@ -27,20 +104,18 @@ namespace maths
   {
   private:
     enum { Mult = maths::max<Long<m1>, Long<m2>>::type::value };
-    enum { Dec = maths::mod
-                 <
-                   typename maths::add<Long<d1>, Long<d2>>::type
-                   , typename maths::pow<Long<10>, Long<Mult>>::type
-                 >::type::value
-         };
+    enum { D = details::double_d_compute<d1, d2, m1, m2, m1 < m2>::type::value };
+    /// Ent = e1 + e2 + d/(10 ^ Mult);
     enum { Ent = maths::add<typename maths::add<Long<e1>, Long<e2>>::type
                             , typename maths::div
                               <
-                                typename maths::add<Long<d1>, Long<d2>>::type
+                                Long<D>
                                 , typename maths::pow<Long<10>, Long<Mult>>::type
                               >::type
                             >::type::value
          };
+    /// Dec = d % (10 ^ Mult);
+    enum { Dec = maths::mod<Long<D>, typename maths::pow<Long<10>, Long<Mult>>::type>::type::value };
   public:
     typedef Double<Ent, Dec, Mult> type;
   };
