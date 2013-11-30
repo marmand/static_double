8541bfba004dcf28e67b8da3b0d59e73956815ac
Armand Leclercq <marman.email@gmail.com>
2013-11-30 17:46:50 +0100

    Make compare in maths namespace

--- compare.hh
+++ compare.hh
@@ -1,99 +1,99 @@
 /*!
 ** \author  armand
-** \file  compare.hh
+** \file  maths/compare.hh
 ** \date  Sun 01 Sep 2013 02:38:30 PM CEST
 */
 
-#ifndef COMPARE_HH_
-# define COMPARE_HH_
+#ifndef MATHS_COMPARE_HH_
+# define MATHS_COMPARE_HH_
 
-template
-<
-  typename lhs
-  , typename rhs
-  , bool first
-  , bool second
-  , bool eq_first
-  , bool eq_second
->
-struct compare {};
-
-template
-<
-  typename lhs
-  , typename rhs
-  , bool second
-  , bool eq_first
-  , bool eq_second
->
-struct compare
-<
-  lhs
-  , rhs
-  , true
-  , second
-  , eq_first
-  , eq_second
->
+namespace maths
 {
-  typedef lhs type;
-};
+  template
+  <
+    typename lhs
+    , typename rhs
+    , bool first
+    , bool second
+    , bool eq_first
+    , bool eq_second
+  >
+  struct compare {};
 
-template
-<
-  typename lhs
-  , typename rhs
-  , bool eq_first
-  , bool eq_second
->
-struct compare
-<
-  lhs
-  , rhs
-  , false
-  , true
-  , eq_first
-  , eq_second
->
-{
-  typedef rhs type;
-};
+  template
+  <
+    typename lhs
+    , typename rhs
+    , bool second
+    , bool eq_first
+    , bool eq_second
+  >
+  struct compare
+  <
+    lhs
+    , rhs
+    , true
+    , second
+    , eq_first
+    , eq_second
+  >
+  {
+    typedef lhs type;
+  };
+  template
+  <
+    typename lhs
+    , typename rhs
+    , bool eq_first
+    , bool eq_second
+  >
+  struct compare
+  <
+    lhs
+    , rhs
+    , false
+    , true
+    , eq_first
+    , eq_second
+  >
+  {
+    typedef rhs type;
+  };
+  template
+  <
+    typename lhs
+    , typename rhs
+    , bool eq_second
+  >
+  struct compare
+  <
+    lhs
+    , rhs
+    , false
+    , false
+    , true
+    , eq_second
+  >
+  {
+    typedef lhs type;
+  };
+  template
+  <
+    typename lhs
+    , typename rhs
+  >
+  struct compare
+  <
+    lhs
+    , rhs
+    , false
+    , false
+    , false
+    , true
+  >
+  {
+    typedef rhs type;
+  };
+} /* maths */
 
-template
-<
-  typename lhs
-  , typename rhs
-  , bool eq_second
->
-struct compare
-<
-  lhs
-  , rhs
-  , false
-  , false
-  , true
-  , eq_second
->
-{
-  typedef lhs type;
-};
-
-template
-<
-  typename lhs
-  , typename rhs
->
-struct compare
-<
-  lhs
-  , rhs
-  , false
-  , false
-  , false
-  , true
->
-{
-  typedef rhs type;
-};
-
-#endif /* !COMPARE_HH_ */
+#endif /* !MATHS_COMPARE_HH_ */
