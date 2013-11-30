6a24e45dc52fdb1136ae3fd09faa9053416da754
Armand Leclercq <marman.email@gmail.com>
2013-11-30 10:19:35 +0100

    Correctly handle negative doubles

--- double/type.hh
+++ double/type.hh
@@ -9,25 +9,79 @@
 # include <cmath>
 # include <digits.hh>
 
+namespace maths
+{
+  namespace double_
+  {
+    namespace type_
+    {
+      template
+      <
+        long e
+        , unsigned long d
+        , unsigned long m
+        , unsigned long size
+        , bool null
+      >
+      struct impl {};
+
+      template
+      <
+        long e
+        , unsigned long d
+        , unsigned long m
+        , unsigned long s
+      >
+      struct impl<e, d, m, s, false>
+      {
+        enum { Ent = e };
+        enum { Dec = d };
+        enum { Mult = m };
+        enum { Sign = e < 0 };
+        operator double() const
+        {
+          return Sign
+            ? Ent - (Dec / std::pow(10.0, Mult))
+            : Ent + (Dec / std::pow(10.0, Mult));
+        }
+      };
+      template
+      <
+        long e
+        , unsigned long d
+        , unsigned long m
+        , unsigned long s
+      >
+      struct impl<e, d, m, s, true>
+      {
+        enum { Ent = e };
+        enum { Dec = d };
+        enum { Mult = m };
+        enum { Sign = s > 1 };
+        operator double() const
+        {
+          return Sign
+            ? Ent - (Dec / std::pow(10.0, Mult))
+            : Ent + (Dec / std::pow(10.0, Mult));
+        }
+      };
+    } /* type_ */
+  } /* double_ */
+} /* maths */
+
 template
 <
   long entiere
   , unsigned long decimal
   , unsigned long mult
+  , unsigned long size
+  , bool null
 >
 struct Double
+  : public maths::double_::type_::impl<entiere, decimal, mult, size, null>
 {
-  enum { Ent = entiere };
-  enum { Dec = decimal };
-  enum { Mult = mult };
-  operator double() const
-  {
-    return Ent < 0
-      ? Ent - (Dec / std::pow(10.0, Mult))
-      : Ent + (Dec / std::pow(10.0, Mult));
-  }
 };
 
-# define DOUBLE(Ent, Dec) Double<Ent, Dec, DIGITS(Dec)>
+# define DOUBLE(Ent, Dec) Double<Ent, Dec, DIGITS(Dec), DIGITS(Ent), Ent == 0>
 
 #endif /* !DOUBLE_TYPE_HH_ */
