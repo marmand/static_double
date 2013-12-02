49d0b2624ed6681a0332bb2e011212ffec4a5c1f
Armand Leclercq <marman.email@gmail.com>
2013-12-02 14:19:47 +0100

    Update mul

--- double/mul.hh
+++ double/mul.hh
@@ -43,10 +43,10 @@ namespace maths
         long e
         , unsigned long d
         , unsigned long m
-        , unsigned long s
         , bool n
+        , bool z
       >
-      struct shifted<Double<e, d, m, s, n>>
+      struct shifted<Double<e, d, m, n, z>>
         : public maths::add
                  <
                    typename maths::mul
@@ -63,9 +63,9 @@ namespace maths
         long e
         , unsigned long d
         , unsigned long m
-        , bool n
+        , bool z
       >
-      struct shifted<Double<e, d, m, true, n>>
+      struct shifted<Double<e, d, m, true, z>>
         : public maths::sub
                  <
                    typename maths::mul
@@ -87,19 +87,19 @@ namespace maths
     , unsigned long d2
     , unsigned long m1
     , unsigned long m2
-    , unsigned long s1
-    , unsigned long s2
     , bool n1
     , bool n2
+    , bool z1
+    , bool z2
   >
-  struct mul<Double<e1, d1, m1, s1, n1>, Double<e2, d2, m2, s2, n2>>
+  struct mul<Double<e1, d1, m1, n1, z1>, Double<e2, d2, m2, n2, z2>>
   {
   private:
     /// CommaLess = (e1 * 10 ^ m1 +/- d1) * (e2 * 10 ^ m2 +/- d2)
     enum { CommaLess = maths::mul
                        <
-                         typename double_::mul_::shifted<Double<e1, d1, m1, s1, n1>>::type
-                         , typename double_::mul_::shifted<Double<e2, d2, m2, s2, n2>>::type
+                         typename double_::mul_::shifted<Double<e1, d1, m1, n1, z1>>::type
+                         , typename double_::mul_::shifted<Double<e2, d2, m2, n2, z2>>::type
                        >::type::value
          };
     /// m = m1 + m2
@@ -121,10 +121,10 @@ namespace maths
                    >::type
                  >::type::value
          };
-    enum { Sign = Ent < 0};
-    enum { Null = Ent == 0 };
+    enum { Neg = Ent < 0};
+    enum { Zero = Ent == 0 };
   public:
-    typedef Double<Ent, Dec, Mult, Sign, Null> type;
+    typedef Double<Ent, Dec, Mult, Neg, Zero> type;
   };
 
   /*!
@@ -136,12 +136,12 @@ namespace maths
     , unsigned long d
     , unsigned long m1
     , unsigned long m2
-    , unsigned long s1
-    , unsigned long s2
     , bool n1
     , bool n2
+    , bool z1
+    , bool z2
   >
-  struct mul<Double<0, 0, m1, s1, n1>, Double<e, d, m2, s2, n2>>
+  struct mul<Double<0, 0, m1, n1, z1>, Double<e, d, m2, n2, z2>>
   {
     typedef DOUBLE(0, 0) type;
   };
@@ -154,12 +154,12 @@ namespace maths
     , unsigned long d
     , unsigned long m1
     , unsigned long m2
-    , unsigned long s1
-    , unsigned long s2
     , bool n1
     , bool n2
+    , bool z1
+    , bool z2
   >
-  struct mul<Double<e, d, m1, s1, n1>, Double<0, 0, m2, s2, n2>>
+  struct mul<Double<e, d, m1, n1, z1>, Double<0, 0, m2, n2, z2>>
   {
     typedef DOUBLE(0, 0) type;
   };
