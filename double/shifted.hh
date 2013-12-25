afa7eccec7a0ab9be5154f4d083b2b82aabd29a5
Armand Leclercq <marman.email@gmail.com>
2013-12-26 00:47:46 +0100

    Create shifted tests

--- double/shifted.hh
+++ double/shifted.hh
@@ -14,6 +14,95 @@
 
 namespace maths
 {
+  namespace double_
+  {
+    namespace shifted_
+    {
+      template
+      <
+        long e
+        , unsigned long d
+        , unsigned long m
+        , bool n
+        , bool z
+      >
+      struct impl
+      {
+      private:
+        typedef typename maths::mul<Long<e>, Long<10>>::type prev_ent;
+        enum { Exp = m - 1 };
+        typedef typename maths::mod<Long<d>, typename maths::pow<Long<10>, Long<Exp>>::type>::type ent_add;
+        typedef typename maths::add<prev_ent, ent_add>::type Ent;
+        typedef typename maths::sub
+                <
+                  Long<d>
+                  , typename maths::mul
+                    <
+                      ent_add
+                      , typename maths::pow<Long<10>, Long<Exp>
+                    >::type
+                  >::type
+                >::type Dec;
+      public:
+        typedef impl<Ent::value, Dec::value, Exp, n, z> type;
+      };
+      template
+      <
+        long e
+        , unsigned long d
+        , unsigned long m
+        , bool z
+      >
+      struct impl<e, d, m, true, z>
+      {
+      private:
+        typedef typename maths::mul<Long<e>, Long<10>>::type prev_ent;
+        enum { Exp = m - 1 };
+        typedef typename maths::mod
+                <
+                  Long<d>
+                  , typename maths::pow
+                    <
+                      Long<10>
+                      , Long<Exp>
+                    >::type
+                >::type ent_add;
+        typedef typename maths::sub<prev_ent, ent_add>::type Ent;
+        typedef typename maths::sub
+                <
+                  Long<d>
+                  , typename maths::mul
+                    <
+                      ent_add
+                      , typename maths::pow<Long<10>, Long<Exp>>::type
+                    >::type
+                >::type Dec;
+      public:
+        typedef impl<Ent::value, Dec::value, Exp, true, z> type;
+      };
+      template
+      <
+        long e
+        , unsigned long m
+        , bool n
+        , bool z
+      >
+      struct impl<e, 0, m, n, z>
+      {
+        typedef Long<e> type;
+      };
+      template
+      <
+        long e
+        , unsigned long m
+        , bool z
+      >
+      struct impl<e, 0, m, true, z>
+      {
+        typedef Long<e> type;
+      };
+    } /* shifted_ */
+  } /* double_ */
   template
   <
     long e
@@ -23,34 +112,7 @@ namespace maths
     , bool z
   >
   struct shifted<Double<e, d, m, n, z>>
-    : public maths::add
-             <
-               typename maths::mul
-               <
-                 Long<e>
-                 , typename maths::pow<Long<10>, Long<m>>::type
-               >::type
-               , Long<d>
-             >
-  {
-  };
-  template
-  <
-    long e
-    , unsigned long d
-    , unsigned long m
-    , bool z
-  >
-  struct shifted<Double<e, d, m, true, z>>
-    : public maths::sub
-             <
-               typename maths::mul
-               <
-                 Long<e>
-                 , typename maths::pow<Long<10>, Long<m>>::type
-               >::type
-               , Long<d>
-             >
+    : public double_::shifted_::impl<e, d, m, n, z>
   {
   };
 } /* maths */
