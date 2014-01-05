e38f99ac8681e4696a07445869295c14a943eb7f
Armand Leclercq <marman.email@gmail.com>
2014-01-05 20:05:27 +0100

    Complete fact code

--- algorithms/fact.hh
+++ algorithms/fact.hh
@@ -7,15 +7,41 @@
 #ifndef MATHS_ALGORITHMS_FACT_HH_
 # define MATHS_ALGORITHMS_FACT_HH_
 # include <maths/long.hh>
+# include <maths/mul.hh>
 
 namespace maths
 {
   namespace algorithms
   {
+    namespace long_
+    {
+      template <typename LongType>
+      struct impl
+      {
+      };
+      template <long v>
+      struct impl<Long<v>>
+      {
+        typedef typename maths::mul
+                <
+                  Long<v>
+                  , typename impl<Long<v - 1>>::type
+                >::type type;
+      };
+      template<>
+      struct impl<Long<0>>
+      {
+        typedef Long<1> type;
+      };
+    } /* long_ */
     template <typename e>
     struct fact
     {
-      typedef e type;
+    };
+    template <long v>
+    struct fact<Long<v>>
+      : public long_::impl<Long<v>>
+    {
     };
   } /* algorithms */
 } /* maths */
