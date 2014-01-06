2217ccf3f09c71fb6931bd247072e6468c4d90d8
Armand Leclercq <marman.email@gmail.com>
2014-01-06 14:40:32 +0100

    Create and test fibonnacci algorithm

---
+++ algorithms/fibo.hh
@@ -0,0 +1,41 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/algorithms/fibo.hh
+ * \date  Mon 06 Jan 2014 02:15:25 PM CET
+ */
+
+#ifndef MATHS_ALGORITHMS_FIBO_HH_
+# define MATHS_ALGORITHMS_FIBO_HH_
+# include <maths/long.hh>
+# include <maths/add.hh>
+
+namespace maths
+{
+  namespace algorithms
+  {
+    template <typename V>
+    struct fibo {};
+
+    template <long v>
+    struct fibo<Long<v>>
+    {
+      typedef typename maths::add
+              <
+                typename maths::algorithms::fibo<Long<v - 1>>::type
+                , typename maths::algorithms::fibo<Long<v - 2 >>::type
+              >::type type;
+    };
+    template<>
+    struct fibo<Long<0>>
+    {
+      typedef Long<0> type;
+    };
+    template<>
+    struct fibo<Long<1>>
+    {
+      typedef Long<1> type;
+    };
+  } /* algorithms */
+} /* maths */
+
+#endif /* !MATHS_ALGORITHMS_FIBO_HH_ */
