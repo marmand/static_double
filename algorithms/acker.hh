ca0cffa943a647823afeb329776b1e0373059c7e
Armand Leclercq <marman.email@gmail.com>
2014-01-06 15:11:08 +0100

    Create Ackermann function

---
+++ algorithms/acker.hh
@@ -0,0 +1,40 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/algorithms/acker.hh
+ * \date  Mon 06 Jan 2014 02:51:53 PM CET
+ */
+
+#ifndef MATHS_ALGORITHMS_ACKER_HH_
+# define MATHS_ALGORITHMS_ACKER_HH_
+# include <maths/long.hh>
+
+namespace maths
+{
+  namespace algorithms
+  {
+    template <typename M, typename N>
+    struct acker {};
+
+    template <long m, long n>
+    struct acker<Long<m>, Long<n>>
+    {
+      typedef typename acker
+              <
+                Long<m - 1>
+                , typename acker<Long<m>, Long<n - 1>>::type
+              >::type type;
+    };
+    template <long m>
+    struct acker<Long<m>, Long<0>>
+    {
+      typedef typename acker<Long<m - 1>, Long<1>>::type type;
+    };
+    template <long n>
+    struct acker<Long<0>, Long<n>>
+    {
+      typedef Long<n + 1> type;
+    };
+  } /* algorithms */
+} /* maths */
+
+#endif /* !MATHS_ALGORITHMS_ACKER_HH_ */
