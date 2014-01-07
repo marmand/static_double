bd1029ed52210f89afd40d84209bef421c8b6199
Armand Leclercq <marman.email@gmail.com>
2014-01-07 11:18:40 +0100

    Create sqrt structure

---
+++ double/sqrt.hh
@@ -0,0 +1,15 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/double/sqrt.hh
+ * \date  Tue 07 Jan 2014 11:16:46 AM CET
+ */
+
+#ifndef MATHS_DOUBLE_SQRT_HH_
+# define MATHS_DOUBLE_SQRT_HH_
+# include <maths/double.hh>
+
+namespace maths
+{
+} /* maths */
+
+#endif /* !MATHS_DOUBLE_SQRT_HH_ */
---
+++ sqrt.hh
@@ -0,0 +1,16 @@
+/*!
+ * \author  Armand Leclercq
+ * \file  maths/sqrt.hh
+ * \date  Tue 07 Jan 2014 11:15:52 AM CET
+ */
+
+#ifndef MATHS_SQRT_HH_
+# define MATHS_SQRT_HH_
+
+namespace maths
+{
+  template <typename T>
+  struct sqrt;
+} /* maths */
+
+#endif /* !MATHS_SQRT_HH_ */
