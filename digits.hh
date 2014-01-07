0939d3e7b9d26076a60a51ca2098fd5d333f9507
Armand Leclercq <marman.email@gmail.com>
2014-01-07 12:59:06 +0100

    Move files to maths library

---
+++ digits.hh
@@ -0,0 +1,10 @@
+/*!
+** \author Armand Leclercq
+** \file maths/digits.hh
+** \date Sat 24 Aug 2013 02:57:03 PM CEST
+*/
+
+#ifndef MATHS_DIGITS_HH_
+# define MATHS_DIGITS_HH_
+# define DIGITS(Val)    (sizeof(#Val) - 1)
+#endif /* !MATHS_DIGITS_HH_ */
