4a9b06c1c9abd4fefca7be89167272d2aca9d8aa
Armand Leclercq <armand@leclercq.site>
2017-10-25 20:47:38 +0000

    Add tests on Double fields

--- tests/double.cc
+++ tests/double.cc
@@ -12,4 +12,9 @@ TEST(Double, create)
   typedef DOUBLE(3, 14) value;
   // Construct and implicit cast to double
   ASSERT_EQ(3.14, value());
+  ASSERT_EQ(3, value::Ent);
+  ASSERT_EQ(14, value::Dec);
+  ASSERT_EQ(2, value::Mult);
+  ASSERT_FALSE(value::Neg);
+  ASSERT_FALSE(value::Zero);
 }
