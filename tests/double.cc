5805c30c2b4c77e023a4e16f890a88178b2fcf8d
Armand Leclercq <armand@leclercq.site>
2017-10-25 20:51:55 +0000

    Add tests for Double values and types

--- tests/double.cc
+++ tests/double.cc
@@ -7,7 +7,19 @@
 
 #include <double.hh>
 
-TEST(Double, create)
+TEST(Double, zero)
+{
+  typedef DOUBLE(0, 0) value;
+  // Construct and implicit cast to double
+  ASSERT_EQ(0, value());
+  ASSERT_EQ(0, value::Ent);
+  ASSERT_EQ(0, value::Dec);
+  ASSERT_EQ(1, value::Mult);
+  ASSERT_FALSE(value::Neg);
+  ASSERT_TRUE(value::Zero);
+}
+
+TEST(Double, pi)
 {
   typedef DOUBLE(3, 14) value;
   // Construct and implicit cast to double
@@ -17,4 +29,22 @@ TEST(Double, create)
   ASSERT_EQ(2, value::Mult);
   ASSERT_FALSE(value::Neg);
   ASSERT_FALSE(value::Zero);
+}
+
+TEST(Double, minus_pi)
+{
+  typedef DOUBLE(-3, 14) value;
+  // Construct and implicit cast to double
+  ASSERT_EQ(-3.14, value());
+  ASSERT_EQ(-3, value::Ent);
+  ASSERT_EQ(14, value::Dec);
+  ASSERT_EQ(2, value::Mult);
+  ASSERT_TRUE(value::Neg);
+  ASSERT_FALSE(value::Zero);
+}
+
+TEST(Double, three_zero_one)
+{
+  typedef DOUBLE(3, 01) value;
+  ASSERT_EQ(3.01, value());
 }
