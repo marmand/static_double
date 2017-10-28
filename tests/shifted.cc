f64a4dc57d13d224d433c3dcf0d28eadc07303e6
Armand Leclercq <armand@leclercq.site>
2017-10-28 09:25:45 +0000

    Add Double null tests

--- tests/shifted.cc
+++ tests/shifted.cc
@@ -10,4 +10,35 @@
 TEST(Shifted, Long)
 {
   ASSERT_EQ(Long<10>(), maths::shifted<Long<10>>::type());
+  ASSERT_EQ(Long<-10>(), maths::shifted<Long<-10>>::type());
+}
+
+TEST(Shifted, Double_Null)
+{
+  {
+    typedef DOUBLE(0, 0) null;
+    typedef maths::shifted<null>::type result;
+    ASSERT_EQ(0, null());
+    ASSERT_EQ(0, result());
+  }
+  {
+    typedef DOUBLE(-0, 0) null;
+    typedef maths::shifted<null>::type result;
+    ASSERT_EQ(0, null());
+    ASSERT_EQ(0, result());
+  }
+  // This case should never occur
+  {
+    typedef DOUBLE(0, -0) null;
+    typedef maths::shifted<null>::type result;
+    ASSERT_EQ(0, null());
+    ASSERT_EQ(0, result());
+  }
+  // This case should never occur
+  {
+    typedef DOUBLE(-0, -0) null;
+    typedef maths::shifted<null>::type result;
+    ASSERT_EQ(0, null());
+    ASSERT_EQ(0, result());
+  }
 }
