c6472ff71bb7019466bef91a272e4e2be975f2f6
Armand Leclercq <armand.leclercq@gmail.com>
2018-06-03 19:33:41 +0200

    Correct location of header files once installed

--- local.mk
+++ local.mk
@@ -1,21 +1,21 @@
 include %reldir%/tests/local.mk
 
-nobase_include_HEADERS += %reldir%/abs.hh
-nobase_include_HEADERS += %reldir%/add.hh
-nobase_include_HEADERS += %reldir%/convert.hh
-nobase_include_HEADERS += %reldir%/compare.hh
-nobase_include_HEADERS += %reldir%/digits.hh
-nobase_include_HEADERS += %reldir%/div.hh
-nobase_include_HEADERS += %reldir%/double.hh
-nobase_include_HEADERS += %reldir%/long.hh
-nobase_include_HEADERS += %reldir%/eq.hh
-nobase_include_HEADERS += %reldir%/max.hh
-nobase_include_HEADERS += %reldir%/min.hh
-nobase_include_HEADERS += %reldir%/mod.hh
-nobase_include_HEADERS += %reldir%/mul.hh
-nobase_include_HEADERS += %reldir%/pow.hh
-nobase_include_HEADERS += %reldir%/shifted.hh
-nobase_include_HEADERS += %reldir%/sub.hh
+nobase_pkginclude_HEADERS += %reldir%/abs.hh
+nobase_pkginclude_HEADERS += %reldir%/add.hh
+nobase_pkginclude_HEADERS += %reldir%/convert.hh
+nobase_pkginclude_HEADERS += %reldir%/compare.hh
+nobase_pkginclude_HEADERS += %reldir%/digits.hh
+nobase_pkginclude_HEADERS += %reldir%/div.hh
+nobase_pkginclude_HEADERS += %reldir%/double.hh
+nobase_pkginclude_HEADERS += %reldir%/long.hh
+nobase_pkginclude_HEADERS += %reldir%/eq.hh
+nobase_pkginclude_HEADERS += %reldir%/max.hh
+nobase_pkginclude_HEADERS += %reldir%/min.hh
+nobase_pkginclude_HEADERS += %reldir%/mod.hh
+nobase_pkginclude_HEADERS += %reldir%/mul.hh
+nobase_pkginclude_HEADERS += %reldir%/pow.hh
+nobase_pkginclude_HEADERS += %reldir%/shifted.hh
+nobase_pkginclude_HEADERS += %reldir%/sub.hh
 
 include %reldir%/long/local.mk
 include %reldir%/double/local.mk
