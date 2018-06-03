13afe3d40a7376d621520177c18844dc46f90926
Armand Leclercq <armand.leclercq@gmail.com>
2018-06-03 13:34:45 +0200

    Add dependencies to all tests targets

--- tests/local.mk
+++ tests/local.mk
@@ -22,18 +22,32 @@ ${builddir}/_googletest/googlemock/gtest/libgtest_main.a: ${builddir}/_googletes
 AM_DEFAULT_SOURCE_EXT = .cc
 
 check_PROGRAMS += %reldir%/abs
+%reldir%/abs.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/add
+%reldir%/add.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/convert
+%reldir%/convert.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/digits
+%reldir%/digits.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/div
+%reldir%/div.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/double
+%reldir%/double.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/eq
+%reldir%/eq.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/max
+%reldir%/max.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/min
+%reldir%/min.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/mod
+%reldir%/mod.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/mul
+%reldir%/mul.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/pow
+%reldir%/pow.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/shifted
+%reldir%/shifted.$(OBJEXT): ${builddir}/googletest
 check_PROGRAMS += %reldir%/sub
+%reldir%/sub.$(OBJEXT): ${builddir}/googletest
 
 TESTS = $(check_PROGRAMS)
