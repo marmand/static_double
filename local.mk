2e87ff3287e603a04094442912e6172ce02acf4c
Armand Leclercq <armand.leclercq@gmail.com>
2018-06-03 13:39:17 +0200

    Add headers to dist in order to have distcheck work properly

--- local.mk
+++ local.mk
@@ -3,9 +3,11 @@ include %reldir%/tests/local.mk
 include_HEADERS += %reldir%/abs.hh
 include_HEADERS += %reldir%/add.hh
 include_HEADERS += %reldir%/convert.hh
+include_HEADERS += %reldir%/compare.hh
 include_HEADERS += %reldir%/digits.hh
 include_HEADERS += %reldir%/div.hh
 include_HEADERS += %reldir%/double.hh
+include_HEADERS += %reldir%/long.hh
 include_HEADERS += %reldir%/eq.hh
 include_HEADERS += %reldir%/max.hh
 include_HEADERS += %reldir%/min.hh
