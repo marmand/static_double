e45f24a67db8e1dd2bb7aead7c03a2ae27f57fec
Armand Leclercq <armand.leclercq@gmail.com>
2018-06-03 19:05:25 +0200

    SUB: Template UnitTest

--- tests/sub.cc
+++ tests/sub.cc
@@ -3,14 +3,31 @@
  * \file: sub.cc
  * \date: Mon 30 Oct 2017 07:50:39 AM UTC
  */
-#include <gtest/gtest.h>
-
 #include <sub.hh>
 
-TEST(Add, Positives)
+#include <gtest/gtest.h>
+
+// T must be a std::pair
+template <typename T>
+class SubTest: public ::testing::Test
 {
-  typedef Long<20> lhs;
-  typedef Long<10> rhs;
-  typedef maths::sub<lhs, rhs>::type result;
-  ASSERT_EQ(10, result());
+  using lhs = typename T::first_type;
+  using rhs = typename T::second_type;
+  using result = typename maths::sub<lhs, rhs>::type;
+protected:
+  lhs lhs_;
+  rhs rhs_;
+  result result_;
+}; // class SubTest
+
+using MyTypes = ::testing::Types
+                  <
+                    std::pair<Long<20>, Long<10>>
+                  >;
+
+TYPED_TEST_CASE(SubTest, MyTypes);
+
+TYPED_TEST(SubTest, Sub)
+{
+  ASSERT_EQ(this->lhs_ - this->rhs_, this->result_);
 }
