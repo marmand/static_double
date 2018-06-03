64ac168a8dbb7e3579bda1a1f3f5941827c784de
Armand Leclercq <armand.leclercq@gmail.com>
2018-06-03 12:21:15 +0200

    Add all headers to archives

--- double/local.mk
+++ double/local.mk
@@ -1,19 +1,10 @@
-include %reldir%/tests/local.mk
-
 include_HEADERS += %reldir%/abs.hh
 include_HEADERS += %reldir%/add.hh
-include_HEADERS += %reldir%/convert.hh
-include_HEADERS += %reldir%/digits.hh
 include_HEADERS += %reldir%/div.hh
-include_HEADERS += %reldir%/double.hh
+include_HEADERS += %reldir%/type.hh
 include_HEADERS += %reldir%/eq.hh
 include_HEADERS += %reldir%/max.hh
 include_HEADERS += %reldir%/min.hh
-include_HEADERS += %reldir%/mod.hh
 include_HEADERS += %reldir%/mul.hh
-include_HEADERS += %reldir%/pow.hh
 include_HEADERS += %reldir%/shifted.hh
 include_HEADERS += %reldir%/sub.hh
-
-include %reldir%/long/local.mk
-include %reldir%/double/local.mk
